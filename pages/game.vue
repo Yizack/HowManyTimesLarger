d0a523ca75c3f6e4c4bab5a19afc41c83c22470a
Yizack Rangel <yizackr@gmail.com>
2023-02-25 16:24:10 -0500

    VS text

--- pages/game.vue
+++ pages/game.vue
@@ -6,7 +6,7 @@ import { faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
   <div v-if="loaded && !loser" class="container-fluid vh-100">
     <div class="row gx-3 gy-5 h-100 p-3 pb-3 pt-5 pt-lg-4">
       <div class="col-lg-6">
-        <div class="border rounded h-100 p-3 p-lg-5 text-center d-flex flex-column align-items-center justify-content-center shadow" :style="`background: url('/images/shapes/${left.code_2}.svg')  center center / cover no-repeat;`">
+        <div class="border border-primary rounded h-100 p-3 p-lg-5 text-center d-flex flex-column align-items-center justify-content-center shadow" :style="`background: url('/images/shapes/${left.code_2}.svg')  center center / cover no-repeat;`">
           <h1>“{{ left.name_en }}”</h1>
           <span>total area is approximately</span>
           <div class="display-1 text-primary-emphasis"><b>{{ fixed(left.km2) }}</b> km²</div>
@@ -14,7 +14,7 @@ import { faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
         </div>
       </div>
       <div class="col-lg-6 mt-3 mt-lg-5">
-        <div class="border rounded h-100 p-3 p-lg-5 text-center d-flex flex-column align-items-center justify-content-center shadow" :style="`background: url('/images/shapes/${right.code_2}.svg')  center center / cover no-repeat;`">
+        <div class="border border-secondary rounded h-100 p-3 p-lg-5 text-center d-flex flex-column align-items-center justify-content-center shadow" :style="`background: url('/images/shapes/${right.code_2}.svg')  center center / cover no-repeat;`">
           <h1>“{{ right.name_en }}”</h1>
           <span>total area is</span>
           <div class="text-uppercase">
@@ -44,6 +44,11 @@ import { faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
       </div>
       <div class="text-center">
         <h5 class="text-dark-emphasis">Score: <span class="text-primary-emphasis">{{ score }}</span></h5>
+      </div>
+    </div>
+    <div class="position-absolute top-50 start-50 translate-middle">
+      <div class="bg-primary d-flex text-white rounded-circle align-items-center justify-content-center shadow mb-5" style="height: 100px; width: 100px; padding: 2%;">
+        <div class="display-5"><b>VS</b></div>
       </div>
     </div>
   </div>
@@ -84,7 +89,7 @@ export default {
       }
     },
     openPage (url) {
-      if (window.confirm("Are you sure you want to leave this page?")) {
+      if (window.confirm("Are you sure you want to leave this page? Your progress will be lost.")) {
         this.$router.push(url);
       }
     }

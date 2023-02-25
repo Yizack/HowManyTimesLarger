8f1d166f4b5b4fb30d96eb96f8abefd610fd327b
Yizack Rangel <yizackr@gmail.com>
2023-02-25 02:44:00 -0500

    center text and align flags to center

--- components/CountryVS.vue
+++ components/CountryVS.vue
@@ -4,20 +4,20 @@ import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <template>
-  <NuxtLink class="card card-body h-100 align-items-center justify-content-center border text-decoration-none" :to="`/compare/${vs[0].code_2}/${vs[1].code_2}/`">
-    <span class="mb-2 text-dark-emphasis"><small><b>{{ vs[0].name_en }}</b></small></span>
-    <div class="row">
+  <NuxtLink class="card card-body p-2 p-md-3 h-100 align-items-center justify-content-center border text-decoration-none text-center text-dark-emphasis" :to="`/compare/${vs[0].code_2}/${vs[1].code_2}/`">
+    <span class="mb-2"><small><b>{{ vs[0].name_en }}</b></small></span>
+    <div class="row align-items-center">
       <div class="col-5">
         <img class="img-fluid rounded-1" :src="`/images/flags/${vs[0].code_2}.svg`" width="50">
       </div>
       <div class="col-2 p-0 text-center">
-        <span class="text-dark-emphasis align-self-center"><FontAwesomeIcon :icon="faArrowRightArrowLeft" /></span>
+        <span class="align-self-center"><FontAwesomeIcon :icon="faArrowRightArrowLeft" /></span>
       </div>
       <div class="col-5">
         <img class="img-fluid rounded-1" :src="`/images/flags/${vs[1].code_2}.svg`" width="50">
       </div>
     </div>
-    <span class="mt-2 text-dark-emphasis"><small><b>{{ vs[1].name_en }}</b></small></span>
+    <span class="mt-2"><small><b>{{ vs[1].name_en }}</b></small></span>
   </NuxtLink>
 </template>
 

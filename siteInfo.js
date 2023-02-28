01b6b34dc101690d91a034bfcf713f0b7616cdef
Yizack Rangel <yizackr@gmail.com>
2023-02-28 07:12:11 -0500

    New domain url

--- siteInfo.js
+++ siteInfo.js
@@ -2,7 +2,7 @@ export default {
   name: "How Many Times Larger",
   name_short: "HowManyTimesLarger",
   description: "Website and Game for easy country size comparisons.",
-  url: "https://howmanytimeslarger.yizack.com",
+  url: "https://howmanytimeslarger.com",
   pages: [
     { name: "Home", url: "/" },
     { name: "Countries", url: "/countries/" },

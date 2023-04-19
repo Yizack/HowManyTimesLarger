24ee61d0329fd5596a2ff1987a368d2ad73228d0
Yizack Rangel <yizackr@gmail.com>
2023-04-19 14:45:27 +0200

    minify and shrink resources

--- updateVersion.js
+++ updateVersion.js
@@ -13,6 +13,22 @@ writeFileSync("config.yaml", `platforms:
   android:
     versionName: ${packageJson.version}
     versionCode: ${generateVersionCode(packageJson.version)}
+    gradle:
+      - file: app/build.gradle
+        target:
+            android:
+              buildTypes:
+                release:
+                  minifyEnabled:
+        replace:
+          minifyEnabled: true
+      - file: app/build.gradle
+        target:
+              android:
+                buildTypes:
+                  release:
+        insert: |
+          shrinkResources true
 `);
 
 console.info("config.yaml created");

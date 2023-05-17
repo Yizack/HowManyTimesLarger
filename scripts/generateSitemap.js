53fd964646ca722437c009321f2778b8ea35c3cc
Yizack Rangel <yizackr@gmail.com>
2023-05-17 19:38:13 +0200

    chore: don't include all compare routes on sitemap

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -1,12 +1,8 @@
 import { writeFileSync } from "fs";
-import { createRequire } from "module";
 import xml from "xml";
 import { siteInfo } from "./../utils/siteInfo.js";
 
 const { url, pages } = siteInfo;
-
-const require = createRequire(import.meta.url);
-const { countries } = require("./../assets/data/countries.json");
 
 const outFile = "public/sitemap.xml";
 const date = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Panama" }));
@@ -16,14 +12,6 @@ const routes = [];
 
 Object.values(pages).forEach((page) => {
   routes.push({ path: page.path, priority: page.path === "/" ? "1.00" : "0.80" });
-});
-
-countries.forEach((left) => {
-  countries.forEach((right) => {
-    if (left.code_2 !== right.code_2) {
-      routes.push({ path: `/compare/${left.code_2}/${right.code_2}/`, priority: "0.50" });
-    }
-  });
 });
 
 const xmlObject = {
